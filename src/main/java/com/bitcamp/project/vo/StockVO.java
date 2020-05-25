9a2153fed39ed29a778b93e748daad5360ca3479
arnika79 <arnika7@naver.com>
2020-05-25 12:44:00 +0900

    STOCK VO 변수생성

--- src/main/java/com/bitcamp/project/vo/StockVO.java
+++ src/main/java/com/bitcamp/project/vo/StockVO.java
@@ -1,5 +1,14 @@
 package com.bitcamp.project.vo;
 
-public class StockVO {
+import java.util.Date;
 
+public class StockVO {
+	private int tno;
+	private int quantity;
+	private int tprice;
+	private String stockName;
+	private String id;
+	private String tcategory;
+	private Date tdatetime;
+	
 }
