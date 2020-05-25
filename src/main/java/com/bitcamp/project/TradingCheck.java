38b8c02008313fde06893b01c0f2ea22caed18e1
ekkkk1 <asdqwek200@naver.com>
2020-05-25 09:51:14 +0900

    stock.txt 실시간 로딩 pom.xml에 jsoup 추가함

--- src/main/java/com/bitcamp/project/TradingCheck.java
+++ src/main/java/com/bitcamp/project/TradingCheck.java
@@ -4,9 +4,11 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+import stockCode.StockName;
+
+@Component
 public class TradingCheck {
 
 	@Autowired
@@ -14,19 +16,20 @@ public class TradingCheck {
 
 	@Scheduled(fixedDelay = 3000)
 	public void TestScheduler() {
-//		StockName sn = new StockName();
-//		try {
-//			sn.start();
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		StockName sn = new StockName();
+		try {
+			sn.start();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+//			throw new RuntimeException("Example to throw a runtime exception");
+		}
 //		StockParsing sp = new StockParsing();
 //		Map<String, Info> info = sp.parsing();
 //		System.out.println(info.get("삼성전자").toString());
 //		if (httpSession.getAttribute("stock") != null)
 //			System.out.println(httpSession.getAttribute("stock"));
-		System.out.println("test");
+//		System.out.println("test");
 
 	}
 
