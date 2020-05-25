7e24383c1a75ebead4f218d60867ed7908dd4050
arnika79 <arnika7@naver.com>
2020-05-25 12:47:11 +0900

    stock vo 생성

--- src/main/java/com/bitcamp/project/vo/StockVO.java
+++ src/main/java/com/bitcamp/project/vo/StockVO.java
@@ -6,9 +6,59 @@ public class StockVO {
 	private int tno;
 	private int quantity;
 	private int tprice;
+	private int stockCode;
 	private String stockName;
 	private String id;
 	private String tcategory;
 	private Date tdatetime;
+	
+	public int getStockCode() {
+		return stockCode;
+	}
+	public void setStockCode(int stockCode) {
+		this.stockCode = stockCode;
+	}
+	public int getTno() {
+		return tno;
+	}
+	public void setTno(int tno) {
+		this.tno = tno;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getTprice() {
+		return tprice;
+	}
+	public void setTprice(int tprice) {
+		this.tprice = tprice;
+	}
+	public String getStockName() {
+		return stockName;
+	}
+	public void setStockName(String stockName) {
+		this.stockName = stockName;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTcategory() {
+		return tcategory;
+	}
+	public void setTcategory(String tcategory) {
+		this.tcategory = tcategory;
+	}
+	public Date getTdatetime() {
+		return tdatetime;
+	}
+	public void setTdatetime(Date tdatetime) {
+		this.tdatetime = tdatetime;
+	}
 	
 }
