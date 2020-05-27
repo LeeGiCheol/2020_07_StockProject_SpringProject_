82a9782a27f66d38642d50fc75c9df62245d1a02
ekkkk1 <asdqwek200@naver.com>
2020-05-27 13:14:41 +0900

    현재가 보합인 경우 에러나는거 수정함

--- src/main/java/stockCode/test.java
+++ src/main/java/stockCode/test.java
@@ -1,48 +1,180 @@
+//
 //package stockCode;
 //
-//import java.io.File;
-//import java.io.FileOutputStream;
-//import java.security.CodeSource;
-//import java.util.Iterator;
+//import java.util.Arrays;
+//import java.util.HashMap;
+//import java.util.Map;
 //
 //import org.jsoup.Jsoup;
 //import org.jsoup.nodes.Document;
 //import org.jsoup.nodes.Element;
 //import org.jsoup.select.Elements;
 //
-//import com.bitcamp.project.TradingCheck;
-//
 //public class test {
-//	public static void main(String[] args) {
+//	public Info parse(String code) {
+////	public static void main(String[] args) {
+//		
+//		//long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
+//		
+//		
+//		
+//		Map<String, Info> info = new HashMap<String, Info>();
 //		try {
-//			long beforeTime = System.currentTimeMillis();
-//				CodeSource codeSource = TradingCheck.class.getProtectionDomain().getCodeSource();
-//		          File jarFile = new File(codeSource.getLocation().toURI().getPath());
-//		          String jarDir = jarFile.getParentFile().getPath();
-//		          FileOutputStream output = new FileOutputStream(jarDir+"/StockTest.txt", true);
-////			OutputStream output = new FileOutputStream("./stock.txt");
-//
-//			for (int i = 1; i <= 32; i++) {
-//				String url = "https://finance.naver.com/sise/sise_market_sum.nhn?&page=" + i; // 크롤링할 url지정
-//				Document doc = null; // Document에는 페이지의 전체 소스가 저장된다
-//
-//				doc = Jsoup.connect(url).get();
-//				Elements element = doc.select("table.type_2");
-//
-//				Iterator<Element> ie1 = element.select("td").iterator();
-//
-//				while (ie1.hasNext()) {
-//					byte[] by = (ie1.next().text() + "ㅇ").getBytes();
-//					output.write(by);
-//				}
-//				long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
-//				long secDiffTime = (afterTime - beforeTime) / 1000; // 두 시간에 차 계산
-//				System.out.println("시간차이(m) : " + secDiffTime);
-//					System.out.println(jarDir);
+//			String url = "https://finance.naver.com/item/main.nhn?code=" + code;
+////			String url = "https://finance.naver.com/item/main.nhn?code=" + "005930";
+//			Document doc = null; // Document에는 페이지의 전체 소스가 저장된다
+//			doc = Jsoup.connect(url).get();
+//			Elements viewLists = doc.select("#middle");
+//			
+//			String stockName = null;
+//			String currentPrice = null;
+//			String before = null;
+//			String upDown = null;
+//			
+//			for (Element viewList : viewLists) {
+//				stockName = viewList.selectFirst("strong").text();
+//				
+//				String currentPriceUp = viewList.select(".no_today").text();
+//				String beforeUp = viewList.select(".no_exday").text();
+////				String currentPriceDown = viewList.select(".no_today .no_down").text();
+////				String beforeDown = viewList.select(".no_exday .no_down").text();
+//				
+////				System.out.println(currentPriceUp);
+////				System.out.println(currentPriceDown);
+//				System.out.println(beforeUp);
+////				System.out.println(beforeDown);
+//				// 상승 하락 보합
+//				
+//				String[] cPrice = currentPriceUp.split(" ");
+//				currentPrice = cPrice[0].replace(",", "");
+//				
+//				String[] bFUD = beforeUp.split(" ");
+//				//System.out.println(Arrays.deepToString(bFUD));
+//				
+//				before = "+" + bFUD[1];
+//				upDown = bFUD[5].replace(".", "") + "%";
+//				// 상승인 경우
+////				if (currentPriceUp != null) {
+////					String[] cPrice = currentPriceUp.split(" ");
+////					currentPrice = cPrice[0].replace(",", "");
+////
+////					String[] bFUD = beforeUp.split(" ");
+////					//System.out.println(Arrays.deepToString(bFUD));
+////
+////					before = "+" + bFUD[1];
+////					upDown = bFUD[5].replace(".", "") + "%";
+////					
+////				// 하락인 경우
+////				} else if(currentPriceDown != null) {
+////					String[] cPrice = currentPriceDown.split(" ");
+////					currentPrice = cPrice[0].replace(",", "");
+////
+////					String[] bFUD = beforeDown.split(" ");
+////					//System.out.println(Arrays.deepToString(bFUD));
+////
+////					before = "-" + bFUD[1];
+////					upDown = bFUD[5].replace(".", "") + "%";
+////			
+////					if (before.contains(",")) {
+////						before = before.replace(",", "");
+////					}
+////				}
+////				
+//				
 //			}
-//
+////
+////			System.out.println(stockName);
+////			System.out.println(currentPrice);
+////			System.out.println(before);
+////			System.out.println(upDown);
+//			
+//			
+//			Info inf = new Info();
+//			inf.setStockName(stockName);
+//			inf.setCurrentPrice(currentPrice);
+//			inf.setBefore(before);
+//			inf.setUpDown(upDown);
+//			
+////			System.out.println(inf);
+////			long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+////			long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
+////			System.out.println("시간차이(m) : "+secDiffTime);
+//			
+//			
+//			return inf;
 //		} catch (Exception e) {
 //			e.printStackTrace();
 //		}
+//		return null;
 //	}
 //}
+
+
+package stockCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class test {
+//	public Info parse(String code) {
+	public static void main(String[] args) {
+		
+		//long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
+        
+		        
+		
+		Map<String, Info> info = new HashMap<String, Info>();
+		try {
+			String url = "https://finance.naver.com/item/main.nhn?code=" + "011760";
+//			String url = "https://finance.naver.com/item/main.nhn?code=" + "005930";
+			Document doc = null; // Document에는 페이지의 전체 소스가 저장된다
+			doc = Jsoup.connect(url).get();
+			Elements viewLists = doc.select("#middle");
+
+			String stockName = null;
+			String currentPrice = null;
+			String before = null;
+			String upDown = null;
+
+			for (Element viewList : viewLists) {
+				stockName = viewList.selectFirst("strong").text();
+
+				String currentPrice_ = viewList.select(".no_today .no_up").text();
+				String before_ = viewList.select(".no_exday").text();
+
+				String[] beforeAfter = before_.split(" ");
+				
+				System.out.println(currentPrice_);
+				
+				
+				upDown = beforeAfter[6] + "%";
+				if(beforeAfter[1].equals("상승")) {
+
+					before = "+" + beforeAfter[2];
+				}
+				else if(beforeAfter[1].equals("하락")) {
+					before = "-" + beforeAfter[2];
+				}
+				else if(beforeAfter[1].equals("보합")) {
+					before = beforeAfter[2];
+					upDown = beforeAfter[5] + "%";
+				}
+				
+				System.out.println(before);
+				System.out.println(upDown);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
