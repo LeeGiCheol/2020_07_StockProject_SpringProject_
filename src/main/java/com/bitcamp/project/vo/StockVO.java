28584076fd24de9ca49897e0d837981cd0004b04
ekkkk1 <asdqwek200@naver.com>
2020-06-01 14:31:20 +0900

    tradinghistory price 컬럼 tprice로 이름변경

--- src/main/java/com/bitcamp/project/vo/StockVO.java
+++ src/main/java/com/bitcamp/project/vo/StockVO.java
@@ -21,91 +21,183 @@ public class StockVO {
 	
 	
 	
-	public int getNewPrice() {
-		return newPrice;
-	}
-	public void setNewPrice(int newPrice) {
-		this.newPrice = newPrice;
-	}
-	public int getNewQuantity() {
-		return newQuantity;
-	}
-	public void setNewQuantity(int newQuantity) {
-		this.newQuantity = newQuantity;
-	}
-	public boolean isModifyALL() {
-		return modifyALL;
-	}
-	public void setModifyALL(boolean modifyALL) {
-		this.modifyALL = modifyALL;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public int getBuysell() {
-		return buysell;
-	}
-	public void setBuysell(int buysell) {
-		this.buysell = buysell;
-	}
-	public int getUno() {
-		return uno;
-	}
-	public void setUno(int uno) {
-		this.uno = uno;
-	}
-	public int getrPrice() {
-		return rPrice;
-	}
-	public void setrPrice(int rPrice) {
-		this.rPrice = rPrice;
-	}
-	public String getStockCode() {
-		return stockCode;
-	}
-	public void setStockCode(String stockCode) {
-		this.stockCode = stockCode;
-	}
+	
 	public int getTno() {
 		return tno;
 	}
+
 	public void setTno(int tno) {
 		this.tno = tno;
 	}
+
+	public int getUno() {
+		return uno;
+	}
+
+	public void setUno(int uno) {
+		this.uno = uno;
+	}
+
+	public int getrPrice() {
+		return rPrice;
+	}
+
+	public void setrPrice(int rPrice) {
+		this.rPrice = rPrice;
+	}
+
 	public int getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
+
 	public int getTprice() {
 		return tprice;
 	}
+
 	public void setTprice(int tprice) {
 		this.tprice = tprice;
 	}
+
+	public int getNewPrice() {
+		return newPrice;
+	}
+
+	public void setNewPrice(int newPrice) {
+		this.newPrice = newPrice;
+	}
+
+	public int getNewQuantity() {
+		return newQuantity;
+	}
+
+
+
+
+
+	public void setNewQuantity(int newQuantity) {
+		this.newQuantity = newQuantity;
+	}
+
+
+
+
+
+	public String getStockCode() {
+		return stockCode;
+	}
+
+
+
+
+
+	public void setStockCode(String stockCode) {
+		this.stockCode = stockCode;
+	}
+
+
+
+
+
 	public String getStockName() {
 		return stockName;
 	}
+
+
+
+
+
 	public void setStockName(String stockName) {
 		this.stockName = stockName;
 	}
+
+
+
+
+
 	public String getId() {
 		return id;
 	}
+
+
+
+
+
 	public void setId(String id) {
 		this.id = id;
 	}
 
+
+
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+
+
+
 	public Date getTdatetime() {
 		return tdatetime;
 	}
+
+
+
+
+
 	public void setTdatetime(Date tdatetime) {
 		this.tdatetime = tdatetime;
 	}
+
+
+
+
+
+	public int getBuysell() {
+		return buysell;
+	}
+
+
+
+
+
+	public void setBuysell(int buysell) {
+		this.buysell = buysell;
+	}
+
+
+
+
+
+	public boolean isModifyALL() {
+		return modifyALL;
+	}
+
+
+
+
+
+	public void setModifyALL(boolean modifyALL) {
+		this.modifyALL = modifyALL;
+	}
+
+
+
+
+
 	@Override
 	public String toString() {
 		return "StockVO [tno=" + tno + ", uno=" + uno + ", rPrice=" + rPrice + ", quantity=" + quantity + ", tprice="
