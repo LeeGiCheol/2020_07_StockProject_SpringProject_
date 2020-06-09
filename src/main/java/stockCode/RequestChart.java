01e37d0e5162116b9bf901adc6cb920bf20780b2
arnika79 <arnika7@naver.com>
2020-06-09 17:53:52 +0900

    차트 끄기

--- src/main/java/stockCode/RequestChart.java
+++ src/main/java/stockCode/RequestChart.java
@@ -13,7 +13,7 @@ import java.sql.ResultSet;
 import java.util.Date;
 
 public class RequestChart {
-	String onOff = "on";
+	String onOff = "off";
 	public void connection(String stockName) {
 		if(onOff.equals("OFF"))
 			return;
