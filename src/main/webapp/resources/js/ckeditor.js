fac09a8a89e61ed2dbbf8a9a216a3c872104caac
arnika79 <arnika7@naver.com>
2020-06-10 18:23:29 +0900

    이미지 클라우드 연결

--- src/main/webapp/resources/js/ckeditor.js
+++ src/main/webapp/resources/js/ckeditor.js
@@ -1,11 +1,19 @@
 
 ClassicEditor
 
-    .create(document.querySelector("#content")) 
+    .create(document.querySelector("#content"), {
+        cloudServices: {
+            tokenUrl: 'https://72301.cke-cs.com/token/dev/OOPLXFC6Yfzk0nDEVWJAdNq7aZ4VsGxnWhbBBhtZP6GBhNq8jkBvbW6EzaHv',
+            uploadUrl: 'https://72301.cke-cs.com/easyimage/upload/'
+        },
+        image: {
+            resizeUnit: 'px'
+        }
+    } )
 
         .then(editor => {
 
-            //console.log(editor);
+            // console.log(editor);
 
         })
 
@@ -14,4 +22,3 @@ ClassicEditor
             console.error(error);
 
         });
-
