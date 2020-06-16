86a509b771af2c68c743aefb7778a550b11a9c13
ekkkk1 <asdqwek200@naver.com>
2020-06-16 18:02:56 +0900

    실시간 뉴스(ajax아님)

--- src/main/java/com/bitcamp/project/view/board/NewsController.java
+++ src/main/java/com/bitcamp/project/view/board/NewsController.java
@@ -6,17 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.bitcamp.project.vo.Info;
 
-import stockCode.News3;
+import stockCode.News;
 
 @Controller
 public class NewsController {
 	
 	@GetMapping("/news")
 	public String newsBoard(Model model) {
-		News3 newsBoard = new News3();
-//		Info news_ = newsBoard.news();
-//		String news = news_.getNews().replace("`", "'");
-//		model.addAttribute("news", news);
+		News newsBoard = new News();
+		Info news_ = newsBoard.news();
+		String news = news_.getNews().replace("`", "'");
+		model.addAttribute("news", news);
 		
 		return "newsboard";
 	}
