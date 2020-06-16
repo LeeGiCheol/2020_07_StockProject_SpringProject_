3cd52678ee808d5cb1702d63be825ecfefcb9853
Kim-Kyoung-Min <zooozp2@gmail.com>
2020-06-16 14:59:45 +0900

    <발표전 급하게 커밋>

--- src/main/java/com/test/tst/LoginController.java
+++ src/main/java/com/test/tst/LoginController.java
@@ -1,73 +1,84 @@
 package com.test.tst;
 
 import java.io.IOException;
+
 import javax.servlet.http.HttpSession;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bitcamp.project.service.SignInService;
+import com.bitcamp.project.vo.UserVO;
 import com.github.scribejava.core.model.OAuth2AccessToken;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class LoginController {
 	/* NaverLoginBO */
 	private NaverLoginBO naverLoginBO;
 	private String apiResult = null;
-
+	
+	@Autowired
+	private SignInService signInService;
+	
 	@Autowired
 	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
 		this.naverLoginBO = naverLoginBO;
 	}
 
-//로그인 첫 화면 요청 메소드
 	@RequestMapping(value = "/naverLogin", method = { RequestMethod.GET, RequestMethod.POST })
 	public String login(Model model, HttpSession session) {
-		/* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */
 		String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
-//https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=sE***************&
-//redirect_uri=http%3A%2F%2F211.63.89.90%3A8090%2Flogin_project%2Fcallback&state=e68c269c-5ba9-4c31-85da-54c16c658125
 		System.out.println("네이버:" + naverAuthUrl);
-//네이버
 		model.addAttribute("url", naverAuthUrl);
-		return "naverLogin";
+		return "redirect:"+model.getAttribute("url");
 	}
 
-//네이버 로그인 성공시 callback호출 메소드
 	@RequestMapping(value = "/callback", method = { RequestMethod.GET, RequestMethod.POST })
 	public String callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session)
 			throws IOException, ParseException {
-		System.out.println("여기는 callback");
+		ModelAndView mav = new ModelAndView();
 		OAuth2AccessToken oauthToken;
 		oauthToken = naverLoginBO.getAccessToken(session, code, state);
-//1. 로그인 사용자 정보를 읽어온다.
 		apiResult = naverLoginBO.getUserProfile(oauthToken); // String형식의 json데이터
 		/**
 		 * apiResult json 구조 {"resultcode":"00", "message":"success",
 		 * "response":{"id":"33666449","nickname":"shinn****","age":"20-29","gender":"M","email":"sh@naver.com","name":"\uc2e0\ubc94\ud638"}}
 		 **/
-//2. String형식인 apiResult를 json형태로 바꿈
 		JSONParser parser = new JSONParser();
 		Object obj = parser.parse(apiResult);
 		JSONObject jsonObj = (JSONObject) obj;
-//3. 데이터 파싱
-//Top레벨 단계 _response 파싱
 		JSONObject response_obj = (JSONObject) jsonObj.get("response");
-//response의 nickname값 파싱
+		
 		String nickname = (String) response_obj.get("nickname");
-		System.out.println(nickname);
-//4.파싱 닉네임 세션으로 저장
-		session.setAttribute("naverLoginUser", nickname); // 세션 생성
-		model.addAttribute("result", apiResult);
-		return "redirect:/mainPage";
+		String id = ((String) response_obj.get("email"));
+		UserVO vo = new UserVO();
+		vo.setId(id);
+		vo = signInService.logIn(vo);
+		
+		if (vo == null) { 
+		  mav.addObject("naverId", id);
+		  mav.addObject("naverNickname", nickname);
+		  mav.setViewName("signup_naver"); 
+		  return "signup_naver";
+		} else {
+			session.setAttribute("loginUser", vo);
+			model.addAttribute("result", apiResult);
+			/* mav.setViewName("redirect:/mainPage"); */
+			return "redirect:/mainPage";
+		}
+		
+		/*
+		 * session.setAttribute("naverLoginUser", nickname); // 세션 생성
+		 		return "redirect:/mainPage";
+ 		*/
 	}
 
 //로그아웃
