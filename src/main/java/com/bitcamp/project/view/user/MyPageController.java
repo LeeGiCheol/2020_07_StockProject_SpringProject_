0c8bcc600c3f4819c38cfeb198a5cee490dcb501
arnika79 <arnika7@naver.com>
2020-06-17 14:26:47 +0900

    랭킹 자동갱신 수정

--- src/main/java/com/bitcamp/project/view/user/MyPageController.java
+++ src/main/java/com/bitcamp/project/view/user/MyPageController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.bitcamp.project.Ranking;
 import com.bitcamp.project.service.MyAccountService;
 import com.bitcamp.project.service.MyPostService;
 import com.bitcamp.project.service.UserInfoService;
