811f572358946a1865e2cf8985dcb809a415601d
Kim-Kyoung-Min <zooozp2@gmail.com>
2020-06-18 19:41:29 +0900

    <마이페이지 클릭시 비밀번호 묻고 이동> <소셜로그인은 비밀번호 없으므로 바로 이동>

--- src/main/java/com/bitcamp/project/view/user/MyPageController.java
+++ src/main/java/com/bitcamp/project/view/user/MyPageController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -101,6 +102,25 @@ public class MyPageController {
 	   return "mypage02";
    }   
 
+	
+   @GetMapping(value="/myPagePwCheck") 
+   public String myPageCheck(HttpSession session) {
+	   if(((UserVO)session.getAttribute("loginUser")).getId().substring(((UserVO)session.getAttribute("loginUser")).getId().length()-1).equals("_")) {
+			return "redirect:/myPage01";
+	   }else {
+     		return "myPageCheckPw"; 
+	   }
+   }
+   @ResponseBody
+   @PostMapping(value="/myPagePwCheck")
+   public String myPageCheckPost(@ModelAttribute("password") String password, HttpSession session) {
+	   UserVO loginUser = (UserVO) session.getAttribute("loginUser");
+	   if(bPasswordEncoder.matches(password, loginUser.getPw())){
+		   return Integer.toString(1);
+	   }else {
+		   return Integer.toString(0);
+	   }
+   }
 
 
 
